e1aa48cf62d7ad2127eb88b435ae7c5eeade4314
Jay Cui <shilongjaycui@gmail.com>
2024-07-29 14:40:50 -0600

    Made the Streamlit app simpler

--- ui/recommended_songs_visualized.py
+++ ui/recommended_songs_visualized.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import streamlit as st
-from typing import List
+import pandas as pd
+from typing import List, Dict
 
 # Make the `/api` folder importable
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -14,6 +15,9 @@ from api.spotify_helper_functions import (  # pylint: disable=C0413
     SPOTIFY_API_CLIENT,
     get_several_artists,
     get_several_tracks,
+    get_recommended_songs_based_on_artists,
+    get_recommended_songs_based_on_other_songs,
+    construct_recommended_songs_dataframe,
 )
 
 
@@ -29,61 +33,50 @@ def display_song(song: Song):
     st.markdown(", ".join(song.artist_names))
 
 
-def get_recommendation_button_styling(seed_name: str) -> str:
-    styling: str = f"""
-        <style>
-        .centered-button {{
-            display: flex;
-            justify-content: center;
-            margin-top: 20px;
-        }}
-        .custom-button {{
-            background-color: red; /* Red background */
-            color: white; /* White text */
-            border: none;
-            border-radius: 5px;
-            padding: 10px 20px;
-            font-size: 16px;
-            font-weight: bold; /* Bold text */
-            cursor: pointer;
-            text-align: center;
-        }}
-        .custom-button:hover {{
-            background-color: darkred; /* Darker red on hover */
-        }}
-        </style>
-        <div class="centered-button">
-            <button class="custom-button" onclick="window.location.href='#';">Recommend songs based on the {seed_name}s</button>
-        </div>
-    """
-    return styling
-
-
+# Artists Section
 st.title("Artists")
 artists: List[Artist] = get_several_artists(
     api_client=SPOTIFY_API_CLIENT,
     spotify_urls=list(SEED_ARTISTS.values()),
 )
-# Create columns
-cols = st.columns(len(artists))
+# Create columns for artists
+cols = st.columns([2 for _ in artists])
 # Display each artist in a column
 for col, artist in zip(cols, artists):
     with col:
         display_artist(artist)
-# Add a styled button below the artist display
-st.markdown(get_recommendation_button_styling(seed_name="artist"), unsafe_allow_html=True)
 
+if st.button('Get recommended songs based on the artists'):
+    recommended_songs: List[Dict] = get_recommended_songs_based_on_artists(
+        api_client=SPOTIFY_API_CLIENT,
+        seed_artists=list(SEED_ARTISTS.values()),
+        recommendation_size=10,
+    )
+    recommended_songs_dataframe: pd.DataFrame = construct_recommended_songs_dataframe(recommended_songs=recommended_songs)
 
+    # Display the DataFrame
+    st.dataframe(recommended_songs_dataframe)
+
+# Songs Section
 st.title("Songs")
 songs: List[Song] = get_several_tracks(
     api_client=SPOTIFY_API_CLIENT,
     spotify_urls=list(SEED_TRACKS.values()),
 )
-# Create columns
-cols = st.columns(len(songs))
-# Display each artist in a column
+# Create columns for songs
+cols = st.columns([1 for _ in songs])
+# Display each song in a column
 for col, song in zip(cols, songs):
     with col:
         display_song(song)
-# Add a styled button below the artist display
-st.markdown(get_recommendation_button_styling(seed_name="song"), unsafe_allow_html=True)
+
+if st.button('Get recommended songs based on the songs'):
+    recommended_songs: List[Dict] = get_recommended_songs_based_on_other_songs(
+        api_client=SPOTIFY_API_CLIENT,
+        seed_tracks=list(SEED_TRACKS.values()),
+        recommendation_size=10,
+    )
+    recommended_songs_dataframe: pd.DataFrame = construct_recommended_songs_dataframe(recommended_songs=recommended_songs)
+
+    # Display the DataFrame
+    st.dataframe(recommended_songs_dataframe)
